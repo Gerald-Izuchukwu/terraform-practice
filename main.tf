b45e81ed304a02509b25faef671dc14d84da0842
gerald-izuchukwu <geraldlouisugwunna@gmail.com>
2024-06-18 11:11:10 +0100

    added create instance resource but i cant ssh into it yet

--- main.tf
+++ main.tf
@@ -6,7 +6,10 @@ variable vpc_cidr_block{}
 variable subnet_cidr_block{}
 variable env_prefix{}
 variable my_ip_address{}
-
+variable instance_type{}
+variable my_public_key {}
+variable image_id {}
+variable key_pair_id{}
 # creating a vpc
 resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
@@ -74,4 +77,47 @@ resource "aws_security_group" "myapp-sg" {
     tags = {
         Name = "${var.env_prefix}-sg"
     }
+}
+
+# data "aws_ami" "latest-amazon-linux-image"{
+#     most_recent = true
+#     owners = ["137112412989"]
+#     filter  {
+#         name = "name"
+#         values = ["al2023-ami-*-x86-64"]
+#     }
+
+#     filter  {
+#         name = "virtualization-type"
+#         values = ["hvm"]
+#     }
+# }
+
+data "aws_key_pair" "ssh-key"{
+    key_pair_id = var.key_pair_id
+    # key_name = "test_key"
+    # public_key = file(var.my_public_key)
+}
+
+output "key_pair_name"{
+    value = data.aws_key_pair.ssh-key.key_name
+}
+
+resource "aws_instance" "myapp-server"{
+    ami = var.image_id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    # key_name = aws_key_pair.ssh-key.key_name
+    key_name = data.aws_key_pair.ssh-key.key_name
+    user_data = file("entry_script.sh")
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
+
+output "ec2_public_ip"{
+    value = aws_instance.myapp-server.public_ip
 }
